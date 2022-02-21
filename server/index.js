00a04b1f2de6601bf085e837b8927c67e29d627e
Viraj <virajj014@gmail.com>
2022-02-22 02:57:33 +0530

    port update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = process.env.PORT || 3000
 const path = require('path')
 const http = require("http");
 const fs = require("fs");
